049a473aee54a650e283259222129b40ec457051
Mikołaj Badura <mibadura@gmail.com>
2023-06-22 23:25:44 +0200

    Implemented helpful printed info and most importantly, modified obstacle detection. Edges are now not obstacles

--- Point_Searching_GOAPwithIDAstar.py
+++ Point_Searching_GOAPwithIDAstar.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import sys
+from shapely.geometry import Point, Polygon, LineString
 
 
 class WorldState:
@@ -94,7 +97,38 @@ class TranspositionTable:
             self.visited[state] = depth
 
 
-def plan_action(world_model, goal, heuristic, transposition_table, max_depth):
+class Obstacles:
+    def __init__(self):
+        self.obstacle_polygons = [
+            Polygon([(-5, 3), (-5, 8), (-3, 8), (-3, 6), (-1, 6), (-1, 4), (-3, 4), (-3, 3)]),
+            Polygon([(-3, 1), (-3, 4), (-1, 4),(-1, 3), (2, 3), (2, 1)]),
+            Polygon([(3, 8), (3, 10), (-7, 10), (-7, 8)])
+        ]
+
+    def is_point_on_polygon(self, point, polygon):
+        point = Point(point)
+        for line in zip(list(polygon.exterior.coords)[:-1], list(polygon.exterior.coords)[1:]):
+            if Point(line[0]).distance(point) + Point(line[1]).distance(point) == LineString(line).length:
+                return True
+        return False
+
+    def is_point_in_polygon(self, point):
+        """
+        Check if a point is inside a polygon.
+
+        Args:
+        point (tuple): (x, y) pair representing the point.
+
+        Returns:
+        bool: True if the point is in the polygon and not on its boundary, False otherwise.
+        """
+        return any(polygon.contains(Point(point)) and not self.is_point_on_polygon(point, polygon) for polygon in self.obstacle_polygons)
+
+    def get_obstacle_polygons(self):
+        return self.obstacle_polygons
+
+
+def plan_action(world_model, goal, heuristic, transposition_table, max_depth,  obstacles = None):
     """
     This function uses Iterative Deepening A* (IDA*) to plan actions.
     It returns the first best action found that meets the cutoff heuristic.
@@ -107,15 +141,20 @@ def plan_action(world_model, goal, heuristic, transposition_table, max_depth):
         # Conduct a depth-limited depth-first search and update cutoff and action
         print(f'\n\n---PLAN ACTION--')
         print(f'Current cutoff: {cutoff}')
-        cutoff, actions = do_depth_first(world_model, goal, heuristic, transposition_table, max_depth, cutoff)
+        cutoff, actions = do_depth_first(world_model, goal, heuristic, transposition_table, max_depth, cutoff, obstacles=obstacles)
         # If an action has been found, return it
         if actions:
+            print("plan_action: action plan returned")
             return actions
     return None
 
 
-def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth, cutoff):
-    print(f'\nNew do_depth_first')
+def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth, cutoff, obstacles = None ):
+    print(f'\ndo_depth_first: New do_depth_first')
+    max_depth += 1
+
+    # if obstacles:
+    #     print(obstacles.get_obstacle_polygons())
     """
     This function performs a depth-limited depth-first search with iterative deepening and returns the smallest cutoff
     encountered and the corresponding best action.
@@ -137,17 +176,24 @@ def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth,
     while current_depth >= 0:
         # If the goal is fulfilled by the current state, return the cutoff and the corresponding action
         if goal.is_fulfilled(states[current_depth]):
-            print('-'*10,' Goal is fulfilled ','-'*10)
+            print('-'*10,f' Goal {states[current_depth]} is fulfilled ','-'*10)
             return cutoff, actions
 
-        # If we're at maximum depth, move back up the tree
-        # print(f'Curr depth before check: {current_depth}')
-        if current_depth >= max_depth-1:
-            current_depth -= 1
-            continue
 
         # Calculate total cost of plan including heuristic estimate
         cost = costs[current_depth] + heuristic.estimate(states[current_depth])
+        print('do_depth_first:',f'\tDepth: {current_depth};\tstate {states[current_depth]};\tcost {cost};\tcutoff: {cutoff}')
+
+        if obstacles.is_point_in_polygon((states[current_depth].x, states[current_depth].y)):
+            print(f'do_depth_first: in polygon! {(states[current_depth].x, states[current_depth].y)}')
+            current_depth -= 1
+            continue
+
+        if current_depth >= max_depth - 1:
+            print(f'do_depth_first: current_depth ({current_depth}) >= max_depth-1 and goal not reached. Decreasing depth')
+            current_depth -= 1
+            continue
+
 
         # If the cost exceeds the cutoff, move back up the tree and update smallest cutoff if necessary
         if cost > cutoff:
@@ -160,7 +206,6 @@ def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth,
 
         # Try the next action
         next_action, next_action_idx = states[current_depth].next_action()
-
         if next_action:
             # print('-'*current_depth,f'Next action on depth {current_depth}')
             # Copy the current state and apply the action to the copy
@@ -176,9 +221,8 @@ def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth,
             # Process the new state if it hasn't been seen before
             if not transposition_table.has(states[current_depth + 1]):
                 current_depth += 1
-                # print('-'*current_depth,f'Increasing depth to {current_depth}')
             else:
-                print(f'State {states[current_depth + 1]} alredy in transpo table')
+                print(f'do_depth_first: state {states[current_depth + 1]} alredy in transpo table')
 
             # Add the new state to the transposition table
             transposition_table.add(states[current_depth + 1], current_depth)
@@ -186,6 +230,7 @@ def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth,
         else:
             # If there are no more actions to try, move back up the tree
             current_depth -= 1
+            print(f'do_depth_first: no more actions: depth decreased')
             # print('-'*current_depth,f'No more actions, decreasing depth to {current_depth}')
 
     # If no action is found after searching all states, return the smallest cutoff encountered
@@ -194,12 +239,17 @@ def do_depth_first(world_model, goal, heuristic, transposition_table, max_depth,
 
 def main():
 
+    #FOR SAVING OUTPUT TO TEXT FILE
+    orig_stdout = sys.stdout
+    f = open('out.txt', 'w')
+    sys.stdout = f
+
     # Create actions
     actions = Action.get_all_actions()
 
     # Initial state
-    start = WorldState(-2, 0, actions)
-    goal = Goal(-4, 7)
+    start = WorldState(0, 0, actions)
+    goal = Goal(-2, 7)
 
     # Heuristic function
     heuristic = Heuristic(goal)
@@ -207,8 +257,10 @@ def main():
     # Create transposition table
     transposition_table = TranspositionTable()
 
+    obstacles = Obstacles()
+
     # Plan action
-    plan = plan_action(start, goal, heuristic, transposition_table, max_depth=13)
+    plan = plan_action(start, goal, heuristic, transposition_table, obstacles = obstacles, max_depth=20)
 
     # If a plan was found, print it
     if plan:
@@ -220,6 +272,10 @@ def main():
     x_positions = [start.x]
     y_positions = [start.y]
     current_state = start
+
+    sys.stdout = orig_stdout
+    f.close()
+
     if plan:
         for action in plan:
             if action is None:
@@ -229,18 +285,29 @@ def main():
             x_positions.append(current_state.x)
             y_positions.append(current_state.y)
 
-
         # Add goal position
         x_positions.append(goal.x)
         y_positions.append(goal.y)
 
-        print(f'x positions: {x_positions}')
+        print(f'Plan length: {len(x_positions)-2}')
+
+        plt.figure(dpi=200)  # Set dpi to 300
+
+        # Create obstacle polygons
+        polygons = obstacles.get_obstacle_polygons()
+        for polygon in polygons:
+            # Obtain x and y coordinates
+            x, y = polygon.exterior.xy
+            plt.fill(x, y, alpha=0.5, label='Obstacle')  # fill with colors
 
         # Create plot
-        plt.plot(x_positions, y_positions, '-', label='Path')
+        plt.plot(x_positions, y_positions, '-', linewidth=2, label='Path')
         plt.scatter([start.x], [start.y], color='g', label='Start')
         plt.scatter([goal.x], [goal.y], color='r', label='Goal')
         plt.grid(True)
+        plt.axis('equal')
+        plt.xticks(range(-10, 10))
+        plt.yticks(range(-1, 15))
         plt.legend()
         plt.show()
 
